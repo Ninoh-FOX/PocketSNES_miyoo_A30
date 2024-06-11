5c39ee6b3d53207a629c5de143bc12f752661035
Ninoh-FOX <ninohfox@gmail.com>
2024-06-11 09:15:02 +0200

    PocketSNES standalone for Miyoo A30

---
+++ Emu/SFC/launch.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cpuclock performance 2 1200 384
+echo $0 $*
+RA_DIR=/mnt/SDCARD/RetroArch
+cd $RA_DIR/
+HOME=$RA_DIR/ $RA_DIR/ra32.miyoo -v -L $RA_DIR/.retroarch/cores/snes9x2005_plus_libretro.so "$1"
---
+++ Emu/SFC/launch5.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cpuclock performance 2 1200 384
+echo $0 $*
+RA_DIR=/mnt/SDCARD/RetroArch
+cd $RA_DIR/
+HOME=$RA_DIR/ $RA_DIR/ra32.miyoo -v -L $RA_DIR/.retroarch/cores/mednafen_supafaust_libretro.so "$1"
